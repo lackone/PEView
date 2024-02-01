cd9c2677415a24068a47d10d8f6d4f8712734851
lackone <lackone@126.com>
2024-02-01 22:55:44 +0800

    修改

--- PE查看器/main.cpp
+++ PE查看器/main.cpp
@@ -7,12 +7,23 @@
 
 HINSTANCE g_hInstance;
 UINT_PTR g_timer;
+//窗口句柄
 HWND hListProcess;
 HWND hListModule;
+//控件类
 ListCtrl lcProcess;
 ListCtrl lcModule;
+//工具对象
 ProcessTools pt;
 Tools tools;
+//进程列表排序
+BOOL pidAsc = TRUE;
+BOOL imageBaseAsc = TRUE;
+BOOL imageSizeAsc = TRUE;
+//最后点击的列索引
+int LastColumnIndex = 1;
+//最后点击的行PID
+DWORD LastRowPID = -1;
 
 /**
  * 更新进程列表
@@ -41,13 +52,73 @@ VOID flushProcessList()
 		_itot_s(it->pid, pidBuf, 32, 10);
 		row[1] = pidBuf;
 
-		_itot_s(it->imageBase, imageBaseBuf, 32, 16);
+		_itot_s(it->imageBase, imageBaseBuf, 32, 10);
 		row[2] = imageBaseBuf;
 
-		_itot_s(it->imageSize, imageSizeBuf, 32, 16);
+		_itot_s(it->imageSize, imageSizeBuf, 32, 10);
 		row[3] = imageSizeBuf;
 
 		lcProcess.AddOneRow(row, 4);
+	}
+}
+
+/**
+ * 部分进程刷新
+ */
+VOID partFlushProcessList()
+{
+	std::vector<ProcessItem> vpi = pt.GetProcessList();
+	std::vector<ProcessItem>::iterator it;
+
+	// 获取ListView控件中的行数
+	int itemCount = ListView_GetItemCount(hListProcess);
+	BOOL flag = false;
+
+	LPCTSTR row[4]{ 0 };
+
+	TCHAR pidBuf[32]{ 0 };
+	TCHAR imageBaseBuf[32]{ 0 };
+	TCHAR imageSizeBuf[32]{ 0 };
+
+	TCHAR pidStr[MAX_PATH]{ 0 };
+	DWORD pid{ 0 };
+
+	//循环判断新获取的进程列表是否已经在ListView中
+	for (it = vpi.begin(); it != vpi.end(); ++it)
+	{
+		flag = false;
+		for (int i = 0; i < itemCount; i++)
+		{
+			memset(pidStr, 0, MAX_PATH * sizeof(TCHAR));
+			ListView_GetItemText(hListProcess, i, 1, pidStr, MAX_PATH);
+			pid = _ttoi(pidStr);
+
+			if (it->pid == pid)
+			{
+				flag = true;
+				break;
+			}
+		}
+		//如果没有，则添加进ListView
+		if (!flag)
+		{
+			memset(pidBuf, 0, 32 * sizeof(TCHAR));
+			memset(imageBaseBuf, 0, 32 * sizeof(TCHAR));
+			memset(imageSizeBuf, 0, 32 * sizeof(TCHAR));
+
+			row[0] = it->szExeFile;
+
+			_itot_s(it->pid, pidBuf, 32, 10);
+			row[1] = pidBuf;
+
+			_itot_s(it->imageBase, imageBaseBuf, 32, 10);
+			row[2] = imageBaseBuf;
+
+			_itot_s(it->imageSize, imageSizeBuf, 32, 10);
+			row[3] = imageSizeBuf;
+
+			lcProcess.AddOneRow(row, 4);
+		}
 	}
 }
 
@@ -87,11 +158,67 @@ INT_PTR CALLBACK AboutDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 }
 
 /**
+ * ListView排序
+ */
+int CALLBACK CompareFunc(LPARAM lParam1, LPARAM lParam2, LPARAM lParamSort)
+{
+	TCHAR data1Str[MAX_PATH]{ 0 };
+	TCHAR data2Str[MAX_PATH]{ 0 };
+
+	int columnIndex = (int)lParamSort;
+
+	// 获取列文本
+	ListView_GetItemText(hListProcess, lParam1, columnIndex, data1Str, MAX_PATH);
+	ListView_GetItemText(hListProcess, lParam2, columnIndex, data2Str, MAX_PATH);
+
+	DWORD data1 = _ttoi(data1Str);
+	DWORD data2 = _ttoi(data2Str);
+
+	if (columnIndex == 1)
+	{
+		return data1 > data2 ? (pidAsc ? 1 : 0) : (pidAsc ? 0 : 1);
+	}
+	else if (columnIndex == 2)
+	{
+		return data1 > data2 ? (imageBaseAsc ? 1 : 0) : (imageBaseAsc ? 0 : 1);
+	}
+	else if (columnIndex == 3)
+	{
+		return data1 > data2 ? (imageSizeAsc ? 1 : 0) : (imageSizeAsc ? 0 : 1);
+	}
+}
+
+/**
  * 定时器
  */
 VOID CALLBACK TimerProc(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
 {
-	flushProcessList();
+	partFlushProcessList();
+	//排序
+	ListView_SortItemsEx(hListProcess, CompareFunc, LastColumnIndex);
+	//设置选中行
+	if (LastRowPID != -1)
+	{
+		TCHAR pidStr[MAX_PATH]{ 0 };
+		DWORD pid{ 0 };
+
+		// 获取ListView控件中的行数
+		int itemCount = ListView_GetItemCount(hListProcess);
+		for (int i = 0; i < itemCount; i++)
+		{
+			memset(pidStr, 0, MAX_PATH * sizeof(TCHAR));
+
+			ListView_GetItemText(hListProcess, i, 1, pidStr, MAX_PATH);
+
+			pid = _ttoi(pidStr);
+
+			if (LastRowPID == pid)
+			{
+				ListView_SetItemState(hListProcess, i, LVIS_SELECTED, LVIS_SELECTED);
+				break;
+			}
+		}
+	}
 }
 
 /**
@@ -121,7 +248,10 @@ INT_PTR CALLBACK MainDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		//更新进程列表
 		flushProcessList();
 
-		//g_timer = SetTimer(hwnd, 1, 5000, TimerProc);
+		//排序
+		ListView_SortItemsEx(hListProcess, CompareFunc, LastColumnIndex);
+
+		g_timer = SetTimer(hwnd, 1, 5000, TimerProc);
 	}
 	return TRUE;
 	case WM_CLOSE:
@@ -133,6 +263,7 @@ INT_PTR CALLBACK MainDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_NOTIFY:
 	{
 		NMHDR* pnmh = (NMHDR*)lParam;
+		//行点击事件
 		if (pnmh->hwndFrom == hListProcess && pnmh->code == NM_CLICK)
 		{
 			LPNMITEMACTIVATE pNMItemActivate = (LPNMITEMACTIVATE)lParam;
@@ -144,9 +275,29 @@ INT_PTR CALLBACK MainDlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 					TCHAR pidStr[MAX_PATH]{ 0 };
 					ListView_GetItemText(hListProcess, nIndex, 1, pidStr, MAX_PATH);
 					DWORD pid = _ttoi(pidStr);
+					LastRowPID = pid;
 					flushModuleList(pid);
 				}
 			}
+		}
+		//列点击事件
+		if (pnmh->hwndFrom == hListProcess && pnmh->code == LVN_COLUMNCLICK)
+		{
+			NMLISTVIEW* pNMListView = (NMLISTVIEW*)lParam;
+			LastColumnIndex = pNMListView->iSubItem;
+			if (pNMListView->iSubItem == 1)
+			{
+				pidAsc = !pidAsc;
+			}
+			else if (pNMListView->iSubItem == 2)
+			{
+				imageBaseAsc = !imageBaseAsc;
+			}
+			else if (pNMListView->iSubItem == 3)
+			{
+				imageSizeAsc = !imageSizeAsc;
+			}
+			ListView_SortItemsEx(hListProcess, CompareFunc, LastColumnIndex);
 		}
 	}
 	return TRUE;
